2c304e8b66ec7c9984f2fe8b48f52d4e9e2f1a38
Taha <tahax.dev@gmail.com>
2022-07-24 21:13:43 +0430

    Add the removeAt method

--- src/myArray.java
+++ src/myArray.java
@@ -12,6 +12,18 @@ public class myArray {
       items[count++] = item;
       }
    }
+   public void removeAt(int index){
+      // WE GONNA VALIDATE THE INDEX 
+      if (index < 0 || index >= count) {
+         System.out.println("The Selected Index is not valide");
+      }
+      // WE GONNA SHIFT ALL THE ITEMS OF THE ARRAY ONE STEP TO LEFT  
+      for (int i = index; i < count; i++) {
+         items[index] = items[index+1];
+      }
+      // WE GONNA DECREMENT THE ARRAY ITEM LENGTH (BECAUSE WE REMOVED ONE ITEM)
+      count--;
+   }
    public void print() {
       for (int i = 0; i < count; i++) {
          System.out.println(items[i]);
